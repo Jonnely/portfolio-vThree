a490fab70f0464973105d0bdbb0744378f08ce34
Jonnel Almodal <almodaljonnel76@gmail.com>
2026-01-01 10:14:17 +0800

    add link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,12 +15,32 @@ export const Footer = () => {
                         <img className="logo-img" src={logo} alt="Logo" />
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
-                        <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
-                        </div>
-                        <p>Copyright 2025. All Rights Reserved</p>
+                    <div className="social-icon">
+                        <a
+                            href="https://www.linkedin.com/in/jonnel-almodal-667703134/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={navIcon1} alt="LinkedIn" />
+                        </a>
+
+                        <a
+                            href="https://www.facebook.com/jonnelalmodal76"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={navIcon2} alt="Facebook" />
+                        </a>
+
+                        <a
+                            href="https://www.instagram.com/jonnelings/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                        <img src={navIcon3} alt="Instagram" />
+                        </a>
+                    </div>
+                    <p>Copyright 2025. All Rights Reserved</p>
                     </Col>
                 </Row>
             </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,11 +46,31 @@ export const NavBar = () => {
                     <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                 </Nav>
                 <span className='navbar-text'>
-                <div className='social-icon'>
-                    <a href="#"><img src={navIcon1} alt="" /></a>
-                    <a href="#"><img src={navIcon2} alt="" /></a>
-                    <a href="#"><img src={navIcon3} alt="" /></a>
-                </div>
+                    <div className="social-icon">
+                    <a
+                        href="https://www.linkedin.com/in/jonnel-almodal-667703134/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={navIcon1} alt="LinkedIn" />
+                    </a>
+
+                    <a
+                        href="https://www.facebook.com/jonnelalmodal76"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={navIcon2} alt="Facebook" />
+                    </a>
+
+                    <a
+                        href="https://www.instagram.com/jonnelings/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={navIcon3} alt="Instagram" />
+                    </a>
+                    </div>
                 <button className='vvd' onClick={() => console.log('connect')}>
                     <span>Let’s Connect</span>
                 </button>
